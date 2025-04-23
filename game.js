5f23a3fb98938e44096bb2a258c0d20f44276fd1
Ana Maria <anamariatxt@gmail.com>
2025-04-23 13:09:06 +0100

    fixing sounds

--- game.js
+++ game.js
@@ -147,7 +147,7 @@ function startOver() {
 
 
 function playSound(name) {
-    var audio = new Audio('/sounds/' + name + '.mp3');
+    var audio = new Audio('sounds/' + name + '.mp3');
     audio.play();
 }
 
